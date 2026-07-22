77c682e6c1da278d14a6db16d604e24e927999a1
Dhruv4643 <dhruvthesiya986@gmail.com>
2026-07-22 13:06:20 +0530

    Time: 1 ms (98.16%), Space: 43.4 MB (53.9%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.java
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -1,16 +1,9 @@
 class Solution {
     public boolean isCircularSentence(String sentence) {
-        String[] str= sentence.split("\\s+");
-        int n= str.length;
-        for(int i=0;i<str.length-1;i++){
-            String s= str[i];
-            String x=str[i+1];
-            if(s.charAt(s.length()-1)!=x.charAt(0)){
-                return false;
-            }
+        char [] ch = sentence.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]==' ') if(ch[i-1]!=ch[i+1]) return false;
         }
-        String first=str[0];
-        String last=str[n-1];
-        return last.charAt(last.length()-1)==first.charAt(0);
+        return ch[0]==ch[ch.length-1];
     }
 }
