377bb3ea27b6ddf8beee0a179a8f9e48f62cff96
Dhruv4643 <dhruvthesiya986@gmail.com>
2026-08-22 08:55:54 +0530

    Time: 38 ms (23.46%), Space: 67.4 MB (97%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,27 +1,43 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
 
-        int left = 0;
-        int right = nums.length - 1;
+        // int left = 0;
+        // int right = nums.length - 1;
+        // int count = 0;
+
+        // while (left < right) {
+        //     int sum = nums[left] + nums[right];
+
+        //     if (sum == k) {
+        //         count++;
+        //         left++;
+        //         right--;
+        //     } 
+        //     else if (sum < k) {
+        //         left++;
+        //     } 
+        //     else {
+        //         right--;
+        //     }
+        // }
+
+        // return count;
+        Map<Integer, Integer> map = new HashMap<>();
         int count = 0;
-
-        while (left < right) {
-            int sum = nums[left] + nums[right];
-
-            if (sum == k) {
+        for(int i = 0; i < nums.length; i++){
+            int res = k - nums[i];
+            if(map.containsKey(res)){
                 count++;
-                left++;
-                right--;
-            } 
-            else if (sum < k) {
-                left++;
-            } 
-            else {
-                right--;
+                if(map.get(res) == 1) map.remove(res);
+                else map.put(res, map.get(res) - 1);
+            }
+            else{
+                map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
             }
         }
-
         return count;
+
+
     }
 }
